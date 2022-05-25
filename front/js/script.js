0f08720197b1eb52c293f3f884c2a2452708d9e1
Sebastien Jost <jost.seb@gmail.com>
2022-05-25 05:44:06 +0200

    script.js - Correct the fetch function - add comments

--- front/js/script.js
+++ front/js/script.js
@@ -1,46 +1,44 @@
-//import fetch from 'node-fetch';
+/* -- Variable Init Declarations -- */
+const getAllProducts = `http://localhost:3000/api/products/`
 
-const apiUrlAllProducts = `http://localhost:3000/api/products/`;
-let items = document.querySelector('#items');
+const apiAsync = async () => {
+  try {
+    let response = await fetch(getAllProducts)
+    let data = await response.json()
 
+    /* -- Init Loop : "As long as the data is long do this" -- */
+    for (let i = 0; i < data.length; i++) {
 
-fetch(apiUrlAllProducts)
-  .then((res) =>
-    res.json()
-      .then((data) => {
-        //Start of the for loop
-        for (let i = 0; i < data.length; i++) {
-          //Creation of the Anchors as child of the section
-          items.appendChild(document.createElement('a'));
-          let anchorNb = document.querySelectorAll('#items a')[i];
-          anchorNb.setAttribute('href', `./product.html?id=${data[i]._id}`)
+      /* -- Creation of the Anchors as child of the section -- */
+      document.querySelector('#items').appendChild(document.createElement('a'));
+      let anchorNb = document.querySelectorAll('#items a')[i]
+      anchorNb.setAttribute('href', `./product.html?id=${data[i]._id}`)
 
-          //Creation of the Articles as child of a
-          anchorNb.appendChild(document.createElement('article'));
-          let articleNb = document.querySelectorAll('#items a article')[i];
+      /* -- Creation of the Articles as child of a -- */
+      anchorNb.appendChild(document.createElement('article'))
+      let articleNb = document.querySelectorAll('#items a article')[i]
 
-          //Creation of the images, title H3 and paragraph as child of Article
-          articleNb.appendChild(document.createElement('img'));
-          articleNb.appendChild(document.createElement('h3'));
-          articleNb.appendChild(document.createElement('p'));
-          let imgNb = document.querySelectorAll('#items img')[i];
-          let titleNb = document.querySelectorAll('#items h3')[i];
-          let paragraphNb = document.querySelectorAll('#items p')[i];
+      /* -- Creation of the images, title H3 and paragraph as child of Article -- */
+      articleNb.appendChild(document.createElement('img'))
+      articleNb.appendChild(document.createElement('h3'))
+      articleNb.appendChild(document.createElement('p'))
+      let imgNb = document.querySelectorAll('#items img')[i]
+      let titleNb = document.querySelectorAll('#items h3')[i]
+      let paragraphNb = document.querySelectorAll('#items p')[i]
 
-          //Setting the content & attributes of the images, title H3 and paragraph
-          imgNb.setAttribute('src', data[i].imageUrl);
-          imgNb.setAttribute('alt', data[i].altTxt);
-          titleNb.setAttribute('class', data[i].name);
-          titleNb.textContent = data[i].name;
-          //paragraphNb.setAttribute('class', data[i].description);
-          paragraphNb.textContent = data[i].description;
-        }
-      })
-  )
-  .catch((error) => {
+      /* -- Setting the content & attributes of the images, title H3 and paragraph -- */
+      imgNb.setAttribute('src', data[i].imageUrl)
+      imgNb.setAttribute('alt', data[i].altTxt)
+      titleNb.setAttribute('class', data[i].name)
+      titleNb.textContent = data[i].name
+      paragraphNb.textContent = data[i].description
+    }
+  }
+  catch {
     console.error(error)
-  });
+    alert(`Erreur ${error.message}, lors du chargement de la page`)
+  }
+}
 
-
-
+apiAsync()
 
