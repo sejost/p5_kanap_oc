2b2acf3cf2fc28108f48fc3d125239aacbfbfbdf
Sebastien Jost <jost.seb@gmail.com>
2022-05-25 09:40:44 +0200

    product.js - Correct the fetch function - add comments

--- front/js/product.js
+++ front/js/product.js
@@ -1,23 +1,12 @@
-let itemImg = document.querySelectorAll('.item__img');
+/* -- Variable Init Declarations -- */
 let currentUrl = new URL(window.location.href);
+
+/* -- Variable Init Declarations for search into the URL and get the product ID selected-- */
 let params = new URLSearchParams(currentUrl.search);
 let pageId = params.get("id");
+const getOneProduct = `http://localhost:3000/api/products/${pageId}`;
 
-const apiUrlOneProduct = `http://localhost:3000/api/products/${pageId}`;
-
-
-const colorsTranslator = (color) => {
-    if (color.includes('/') == true) {
-        const colorsSplitted = color.split('/');
-        const color1 = colorsDictionnary[colorsSplitted[0].toLowerCase()];
-        const color2 = colorsDictionnary[colorsSplitted[1].toLowerCase()];
-        return color1 + ' / ' + color2;
-    }
-    else {
-        return colorsDictionnary[color.toLowerCase()];
-    }
-};
-
+/* -- Variable Dictionnary of english colors to french -- */
 const colorsDictionnary = {
     'blue': 'Bleu',
     'white': 'Blanc',
@@ -34,74 +23,114 @@ const colorsDictionnary = {
     'orange': 'Orange'
 };
 
+/* -- Function that convert the color format and translate it into french -- */
+const colorsTranslator = (color) => {
+    if (!color.includes('/')) {
+        return colorsDictionnary[color.toLowerCase()];
+    }
+    else {
+        /* -- If the colors contain a '/' translate separatly each color then add each other into a new translated string-- */
+        const colorsSplitted = color.split('/');
+        let colors = [];
+        let colorsAdded;
+        for (i = 0; i < colorsSplitted.length; i++) {
+            colors[i] = colorsDictionnary[colorsSplitted[i].toLowerCase()];
+            colorsAdded = ` ${colors[i - 1]} / ${colors[i]}`;
+        }
+        return colorsAdded;
+    }
+};
 
+/* -- Main function calling the API-- */
+const apiAsync = async () => {
+    try {
+        let response = await fetch(getOneProduct);
+        let data = await response.json();
 
-fetch(apiUrlOneProduct)
-    .then((res) =>
-        res.json()
-            .then((data) => {
-                document.querySelector('article .item__img').appendChild(document.createElement('img'));
-                let itemImg = document.querySelector('.item__img img');
-                itemImg.setAttribute('src', data.imageUrl)
-                itemImg.setAttribute('alt', data.altTxt);
+        /* -- Creation HTML architecture -- */
+        document.querySelector('article .item__img').appendChild(document.createElement('img'));
+        let itemImg = document.querySelector('.item__img img');
+        itemImg.setAttribute('src', data.imageUrl)
+        itemImg.setAttribute('alt', data.altTxt);
 
-                document.querySelector('#title').textContent = data.name;
-                document.querySelector('#price').textContent = data.price;
-                document.querySelector('#description').textContent = data.description;
+        document.querySelector('#title').textContent = data.name;
+        document.querySelector('#price').textContent = data.price;
+        document.querySelector('#description').textContent = data.description;
 
-                for (let i = 0; i < data.colors.length; i++) {
-                    document.querySelector('#colors').appendChild(document.createElement('option'));
-                    document.querySelectorAll('#colors option')[i + 1].setAttribute('value', data.colors[i]);
-                    document.querySelectorAll('#colors option')[i + 1].textContent = colorsTranslator(data.colors[i]);
+        /* -- Loop create adding multiple colors selector -- */
+        for (let i = 0; i < data.colors.length; i++) {
+            document.querySelector('#colors').appendChild(document.createElement('option'));
+            document.querySelectorAll('#colors option')[i + 1].setAttribute('value', data.colors[i]);
+            document.querySelectorAll('#colors option')[i + 1].textContent = colorsTranslator(data.colors[i]);
+        }
+
+        /* -- Function to remove the message "SVP, choississez une couleur"-- */
+        const removeFirstSelect = () => {
+            document.querySelector('#colors').removeChild((document.querySelectorAll('#colors option'))[0]);
+            document.querySelector('#colors').removeEventListener('click', removeFirstSelect);
+        };
+
+        /* -- Event listener triggerded on click on the select color and callback the remove select function -- */
+        document.querySelector('#colors').addEventListener('click', removeFirstSelect);
+
+        /* -- Event listener triggerded on click on button "ajouter au panier" -- 
+           -- Push information in an array the cart information and add it into the local storage then display a confirmation message --*/
+        document.querySelector('#addToCart').addEventListener('click', () => {
+            let cart = [];
+            let cartValue = document.querySelector('#quantity').value;
+            let colorValue = document.querySelector('#colors').value;
+            let keyName = data.name + ' ' + colorValue;
+
+            /* -- Function create a cart with new products-- */
+            const newCart = () => {
+                cart.push(pageId);
+                cart.push(colorValue);
+                cart.push(cartValue);
+                localStorage.setItem(data.name + ' ' + colorValue, cart);
+                displayResult();
+            }
+
+            /* -- Function to add a new quantity of products in an existing product item cart-- */
+            const addToCart = () => {
+                let idValues = localStorage.getItem(keyName);
+                const cartItemSplitted = idValues.split(',');
+                localStorage.removeItem(keyName)
+                cart.push(pageId);
+                cart.push(colorValue);
+                cart.push(parseInt(cartValue) + parseInt(cartItemSplitted[2]));
+                localStorage.setItem(keyName, cart);
+                displayResult();
+            }
+
+            /* -- Function to display the confirmation adding to cart message-- */
+            const displayResult = () => {
+                if (cartValue > 1) {
+                    alert(`Vos ${cartValue} articles ont bien été ajoutés au panier`)
                 }
+                else {
+                    alert(`Votre article a bien été ajouté au panier`)
+                }
+            }
 
-                const removeFirstSelect = () => {
-                    document.querySelector('#colors').removeChild((document.querySelectorAll('#colors option'))[0]);
-                    document.querySelector('#colors').removeEventListener('click', removeFirstSelect);
-                };
-
-                document.querySelector('#colors').addEventListener('click', removeFirstSelect);
-
-                document.querySelector('#addToCart').addEventListener('click', () => {
-                    let cart = [];
-                    let cartValue = document.querySelector('#quantity').value;
-                    let colorValue = document.querySelector('#colors').value;
-                    let keyName = data.name + ' ' + colorValue;
-                    const addNewCart = () => {
-                        cart.push(pageId);
-                        cart.push(colorValue);
-                        cart.push(cartValue);
-                        localStorage.setItem(data.name + ' ' + colorValue, cart);
-                        alert(`Vos ${cartValue} articles ont bien été ajouté au panier`)
-                    }
-
-                    const addSomeCart = () => {
-                        let idValues = localStorage.getItem(keyName);
-                        const cartItemSplitted = idValues.split(',');
-                        localStorage.removeItem(keyName)
-                        cart.push(pageId);
-                        cart.push(colorValue);
-                        cart.push(parseInt(cartValue) + parseInt(cartItemSplitted[2]));
-                        localStorage.setItem(keyName, cart);
-                        alert(`Vos ${cartValue} articles ont bien été ajouté au panier`)
-                    }
-
-                    if (cartValue == 0 || cartValue > 100 || cartValue < 0) {
-                        alert(`Merci d'indiquer un nombre d'article compris entre 1 et 100`);
-                    }
-                    else if (colorValue == '') {
-                        alert(`Merci de préciser la couleur souhaitée de l'article`);
-                    }
-                    else if (localStorage.getItem(keyName) != null) {
-                        addSomeCart();
-
-                    }
-                    else {
-                        addNewCart();
-                    }
-                })
-            }))
-
-    .catch((error) => {
+            /* -- Main condition in the event listener to be authorized to add product in a cart -- */
+            if (cartValue == 0 || cartValue > 100 || cartValue < 0) {
+                alert(`Merci d'indiquer un nombre d'article compris entre 1 et 100`);
+            }
+            else if (colorValue == '') {
+                alert(`Merci de préciser la couleur souhaitée de l'article`);
+            }
+            else if (localStorage.getItem(keyName) != null) {
+                addToCart();
+            }
+            else {
+                newCart();
+            }
+        })
+    }
+    catch {
         console.error(error)
-    });
+        alert(`Erreur : ${error.message}, lors du chargement de la page`)
+    }
+}
+apiAsync()
+
--- front/js/script.js
+++ front/js/script.js
@@ -1,44 +1,46 @@
 /* -- Variable Init Declarations -- */
-const getAllProducts = `http://localhost:3000/api/products/`
+const getAllProducts = `http://localhost:3000/api/products/`;
 
+/* -- Main function calling the API-- */
 const apiAsync = async () => {
   try {
-    let response = await fetch(getAllProducts)
-    let data = await response.json()
+    let response = await fetch(getAllProducts);
+    let data = await response.json();
 
     /* -- Init Loop : "As long as the data is long do this" -- */
     for (let i = 0; i < data.length; i++) {
 
       /* -- Creation of the Anchors as child of the section -- */
       document.querySelector('#items').appendChild(document.createElement('a'));
-      let anchorNb = document.querySelectorAll('#items a')[i]
-      anchorNb.setAttribute('href', `./product.html?id=${data[i]._id}`)
+      let anchorNb = document.querySelectorAll('#items a')[i];
+      anchorNb.setAttribute('href', `./product.html?id=${data[i]._id}`);
 
       /* -- Creation of the Articles as child of a -- */
-      anchorNb.appendChild(document.createElement('article'))
-      let articleNb = document.querySelectorAll('#items a article')[i]
+      anchorNb.appendChild(document.createElement('article'));
+      let articleNb = document.querySelectorAll('#items a article')[i];
 
       /* -- Creation of the images, title H3 and paragraph as child of Article -- */
-      articleNb.appendChild(document.createElement('img'))
-      articleNb.appendChild(document.createElement('h3'))
-      articleNb.appendChild(document.createElement('p'))
-      let imgNb = document.querySelectorAll('#items img')[i]
-      let titleNb = document.querySelectorAll('#items h3')[i]
-      let paragraphNb = document.querySelectorAll('#items p')[i]
+      articleNb.appendChild(document.createElement('img'));
+      articleNb.appendChild(document.createElement('h3'));
+      articleNb.appendChild(document.createElement('p'));
+      let imgNb = document.querySelectorAll('#items img')[i];
+      let titleNb = document.querySelectorAll('#items h3')[i];
+      let paragraphNb = document.querySelectorAll('#items p')[i];
 
       /* -- Setting the content & attributes of the images, title H3 and paragraph -- */
-      imgNb.setAttribute('src', data[i].imageUrl)
-      imgNb.setAttribute('alt', data[i].altTxt)
-      titleNb.setAttribute('class', data[i].name)
-      titleNb.textContent = data[i].name
-      paragraphNb.textContent = data[i].description
+      imgNb.setAttribute('src', data[i].imageUrl);
+      imgNb.setAttribute('alt', data[i].altTxt);
+      titleNb.setAttribute('class', data[i].name);
+      titleNb.textContent = data[i].name;
+      paragraphNb.textContent = data[i].description;
     }
   }
   catch {
-    console.error(error)
-    alert(`Erreur ${error.message}, lors du chargement de la page`)
+    console.error(error);
+    alert(`Erreur : ${error.message}, lors du chargement de la page`);
   }
 }
 
-apiAsync()
+/* -- Automatic Execution of the function -- */
+apiAsync();
 
